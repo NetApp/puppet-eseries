c43e6a3f47c01858090944e28e9f361954efe4d3
Andrzej Skupien <askupien@mirantis.com>
2015-02-10 17:08:14 +0100

    Tests of volume type

---
+++ spec/puppet/type/netapp_e_volume_spec.rb
@@ -0,0 +1,90 @@
+require 'spec/spec_helper'
+require 'spec/support/shared_examples_for_types'
+
+describe Puppet::Type.type(:netapp_e_volume) do
+  before :each do
+    @volume = { :name => 'volume' }
+    described_class.stubs(:defaultprovider).returns providerclass
+  end
+
+  let :resource do
+    @volume
+  end
+
+  let :providerclass do
+    described_class.provide(:fake_storage_system_provider) { mk_resource_methods }
+  end
+
+  it 'should have :name be its namevar' do
+    described_class.key_attributes.should == [:name]
+  end
+
+  describe 'when validating attributes' do
+    [:storagesystem, :storagepool, :sizeunit, :size, :segsize, :dataassurance, :thin, :repositorysize, :maxrepositorysize,
+     :owningcontrollerid, :growthalertthreshold, :defaultmapping, :expansionpolicy, :cachereadahead].each do |param|
+      it "should have a #{param} parameter" do
+        described_class.attrtype(param).should == :param
+      end
+    end
+
+    [:id, :poolid].each do |prop|
+      it "should have a #{prop} property" do
+        described_class.attrtype(prop).should == :property
+      end
+    end
+  end
+
+  describe 'when validating values' do
+    context 'for name' do
+      it_behaves_like 'a string param/property', :name, true
+    end
+    context 'for id' do
+      it_behaves_like 'a string param/property', :id, true
+    end
+    context 'for storagesystem' do
+      it_behaves_like 'a string param/property', :storagesystem, true
+    end
+    context 'for storagepool' do
+      it_behaves_like 'a string param/property', :storagepool, true
+    end
+    context 'for poolid' do
+      it_behaves_like 'a string param/property', :poolid, true
+    end
+    context 'for sizeunit' do
+      it_behaves_like 'a enum param/property', :sizeunit, %w(bytes b kb mb gb tb pb eb zb yb)
+    end
+    context 'for size' do
+      it_behaves_like 'a string param/property', :size, true
+    end
+    context 'for segsize' do
+      it_behaves_like 'a string param/property', :segsize, true
+    end
+    context 'for dataassurance' do
+      it_behaves_like 'a boolish param/property', :dataassurance, :false
+    end
+    context 'for thin' do
+      it_behaves_like 'a boolish param/property', :thin, :false
+    end
+    context 'for repositorysize' do
+      it_behaves_like 'a string param/property', :repositorysize, true
+    end
+    context 'for maxrepositorysize' do
+      it_behaves_like 'a string param/property', :maxrepositorysize, true
+    end
+    context 'for owningcontrollerid' do
+      it_behaves_like 'a string param/property', :owningcontrollerid, true
+    end
+    context 'for growthalertthreshold' do
+      it_behaves_like 'a string param/property', :growthalertthreshold, true
+    end
+    context 'for defaultmapping' do
+      it_behaves_like 'a boolish param/property', :defaultmapping, :false
+    end
+    context 'for expansionpolicy' do
+      it_behaves_like 'a enum param/property', :expansionpolicy, %w(unknown manual automatic __UNDEFINED), 'automatic'
+    end
+    context 'for cachereadahead' do
+      it_behaves_like 'a boolish param/property', :cachereadahead, :false
+    end
+  end
+end
---
+++ spec/support/shared_examples_for_types.rb
@@ -0,0 +1,70 @@
+shared_examples 'a string param/property' do |param_name, special_characters|
+  it 'should support letters' do
+    value = ('a'..'z').to_a.join ''
+    resource[param_name] = value
+    described_class.new(resource)[param_name].should == value
+  end
+  it 'should support digits' do
+    value = ('0'..'9').to_a.join ''
+    resource[param_name] = value
+    described_class.new(resource)[param_name].should == value
+  end
+  it 'should support underscore' do
+    value = '__'
+    resource[param_name] = value
+    described_class.new(resource)[param_name].should == value
+  end
+  if special_characters
+    it 'should support special characters and spaces' do
+      '!£§!@#$%^&*()-+=[]{};\':"\|?/.>,<~` '.split('').each do |char|
+        value = 'my' + char + 'name'
+        resource[param_name] = value
+        described_class.new(resource)[param_name].should == value
+      end
+    end
+  else
+    it 'should not support special characters and spaces' do
+      '!£§!@#$%^&*()-+=[]{};\':"\|?/.>,<~` '.split('').each do |char|
+        resource[param_name] = char
+        expect { described_class.new(resource) }.to raise_error Puppet::ResourceError
+      end
+    end
+  end
+end
+
+shared_examples 'a enum param/property' do |param_name, enum, default|
+  context 'should acccept' do
+    enum.each do |val|
+      it "#{val}" do
+        resource[param_name] = val
+        expected = val
+        if val == true
+          expected = :true
+        elsif val == false
+          expected = :false
+        elsif val.instance_of?(String)
+          expected = val.to_sym
+        end
+        described_class.new(resource)[param_name].should == expected.to_sym
+      end
+    end
+  end
+  describe 'should not accept' do
+    [1, 'string', :symbol, true, :false, ['array'], { 'hash' => 'value' }].each do |val|
+      next if enum.include? val
+      it "#{val}" do
+        resource[param_name] = val
+        expect { described_class.new(resource) }.to raise_error Puppet::ResourceError
+      end
+    end
+  end
+  unless default.nil?
+    it "should have default value set to #{default}" do
+      described_class.new(resource)[param_name].should == default.to_sym
+    end
+  end
+end
+
+shared_examples 'a boolish param/property' do |param_name, default|
+  include_examples 'a enum param/property', param_name, [true, :true, false, :false], default
+end
