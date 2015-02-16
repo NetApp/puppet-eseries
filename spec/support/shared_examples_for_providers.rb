75e884df8c6c3a92fc1c49963048f92433398a07
Andrzej Skupien <askupien@mirantis.com>
2015-02-16 10:09:45 +0100

    Tests of storage pool provider

--- spec/support/shared_examples_for_providers.rb
+++ spec/support/shared_examples_for_providers.rb
@@ -1,8 +1,15 @@
 shared_examples 'a method with error handling' do |transport_method, provider_method|
   it 'should raise Puppet::Error when something went wrong' do
+    begin
+      provider = resource.provider
+      m = resource.provider.method(provider_method)
+    rescue NameError
+      # it's class method then
+      provider = described_class
+      m = described_class.method(provider_method)
+    end
     expect(@transport).to receive(transport_method).and_raise('some message')
-    allow(described_class).to receive(:transport) { @transport }
-    m = resource.provider.method(provider_method)
+    allow(provider).to receive(:transport) { @transport }
     expect { m.call }.to raise_error(Puppet::Error, 'some message')
   end
 end
